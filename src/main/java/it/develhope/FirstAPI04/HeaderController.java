e367504d0ec50b78673dbfc8330f288f838ba023
Karina Kosovan <karikosovan@gmail.com>
2022-10-06 10:48:40 +0200

    Functioning state

---
+++ src/main/java/it/develhope/FirstAPI04/HeaderController.java
@@ -0,0 +1,18 @@
+package it.develhope.FirstAPI04;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/headers")
+public class HeaderController {
+
+    @GetMapping
+    public String getHeaders(HttpServletRequest request) {
+        return request.getServerName() + ":"
+                + request.getServerPort();
+    }
+}
